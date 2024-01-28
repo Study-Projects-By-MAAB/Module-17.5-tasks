fc50e890813cf71aa9d3db0530581c0b735632d6
maabfw <maabfw@proton.me>
2024-01-28 11:03:51 +0600

    task3 done

---
+++ task3.js
@@ -0,0 +1,7 @@
+let books = ['bangla book', 'english book', 'math book', 'science book', 'islam book']
+if (books.includes('javascript book')){
+    console.log('it is here')
+}
+else{
+    console.log('no its not here')
+}
