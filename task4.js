b75f5ce55533e817d994c0d489aaf8d44506872e
maabfw <maabfw@proton.me>
2024-01-28 11:09:25 +0600

    task4 done

---
+++ task4.js
@@ -0,0 +1,9 @@
+let firstVar = [5,64,84,84,64]
+let secondVar = true
+let date = ('28/01/24')
+let books = ['bangla book', 'english book', 'math book', 'science book', 'islam book']
+
+console.log(Array.isArray(firstVar))
+console.log(Array.isArray(secondVar))
+console.log(Array.isArray(date))
+console.log(Array.isArray(books))
