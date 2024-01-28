32d4a41781dfdd5c63517f00261d3a478c0fbe79
maabfw <maabfw@proton.me>
2024-01-28 10:56:36 +0600

    task2 done

---
+++ task2.js
@@ -0,0 +1,6 @@
+let tourSpot = ['sajek', "cox'sbazar", 'saint martin']
+tourSpot.push('khgrachori')
+// console.log(tourSpot)
+tourSpot.push('sundarbon', 'rangamati')
+tourSpot.pop('rangamati')
+console.log(tourSpot)
