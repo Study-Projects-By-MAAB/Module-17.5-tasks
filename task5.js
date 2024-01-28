f79fbc990158fba64a235f14d9f244b4bfe342c2
maabfw <maabfw@proton.me>
2024-01-28 11:20:00 +0600

    task5 done

---
+++ task5.js
@@ -0,0 +1,6 @@
+let books = ['bangla book', 'english book', 'math book', 'science book', 'islam book']
+let numbers = [5,64,84,84,64]
+let combineArray = books.concat(numbers)
+console.log(books)
+console.log(numbers)
+console.log(combineArray)
